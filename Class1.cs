8958a23bc9d9432b7b02e575793e2a9895a0c65d
assaultassault <144154333+assaultassault@users.noreply.github.com>
2026-05-03 14:04:45 +0500

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,20 @@
+﻿namespace CalculatorIsmailov.Lib
+{
+    public class Class1
+    {
+        public double Add(double a, double b) => a + b;
+
+        public double Subtract(double a, double b) => a - b;
+
+        public double Multiply(double a, double b) => a * b;
+
+        public double Divide(double a, double b)
+        {
+            if (b == 0)
+                throw new DivideByZeroException();
+            return a / b;
+        }
+
+        public double Power(double a, double b) => Math.Pow(a, b);
+    }
+}
